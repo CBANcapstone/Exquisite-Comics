d69ea4a9ae4beabb6078c5f35ddce5d5fff2189e
Mykola Maslov <kategorychnost@gmail.com>
2018-06-21 15:54:17 -0400

    added modules firebase config, Login.js

---
+++ config/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyDW4x3Cvt28hIG3RV9KR4i7vKOBZ5ht-cg",
+  authDomain: "exquisite-comics.firebaseapp.com",
+  databaseURL: "https://exquisite-comics.firebaseio.com",
+  projectId: "exquisite-comics",
+  storageBucket: "exquisite-comics.appspot.com",
+  messagingSenderId: "106821778451"
+};
